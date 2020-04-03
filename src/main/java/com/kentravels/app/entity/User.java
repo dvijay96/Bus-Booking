7826141a28e15d24445adb4710b2d934ed23346d
dvijay96 <dvijay96@gmail.com>
2020-04-03 14:24:50 +0530

    user

--- src/main/java/com/kentravels/app/entity/User.java
+++ src/main/java/com/kentravels/app/entity/User.java
@@ -9,6 +9,8 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Temporal;
@@ -40,7 +42,8 @@ public class User {
 
 	private String password;
 
-	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@ManyToOne(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Role role;
 
 	public int getUserId() {
