fffeeb716e62d6d2efe1473d7cbe6fea14d3a12e
Digvijay Manoj Parit <EE211385@sasken.com>
2020-04-03 12:40:18 +0530

    remote

--- src/main/java/com/kentravels/app/main/BusBookingApplication.java
+++ src/main/java/com/kentravels/app/main/BusBookingApplication.java
@@ -2,8 +2,10 @@ package com.kentravels.app.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages="com.kentravels.app")
+@EntityScan("com.kentravels.app.entity")
 public class BusBookingApplication {
 
 	public static void main(String[] args) {
