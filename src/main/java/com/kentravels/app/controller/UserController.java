414d9c4348b900560a609f39192258d7556fb3cd
Digvijay Manoj Parit <EE211385@sasken.com>
2020-04-06 14:35:51 +0530

    remote

--- src/main/java/com/kentravels/app/controller/UserController.java
+++ src/main/java/com/kentravels/app/controller/UserController.java
@@ -2,6 +2,7 @@ package com.kentravels.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kentravels.app.dto.Customer;
@@ -14,7 +15,7 @@ public class UserController {
 	private UserService service;
 	
 	@PostMapping("/user/register")
-	public String registerUser(Customer customer) {
+	public String registerUser(@RequestBody Customer customer) {
 		service.addUser(customer);
 		return "user registered successfuly";
 	}
