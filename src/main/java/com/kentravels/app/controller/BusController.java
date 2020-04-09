05720a7a081e30d60f9e332de5eb455d3d1bfe1e
dvijay96 <dvijay96@gmail.com>
2020-04-09 15:59:11 +0530

    pc

--- src/main/java/com/kentravels/app/controller/BusController.java
+++ src/main/java/com/kentravels/app/controller/BusController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kentravels.app.dto.BusInfo;
@@ -72,7 +73,7 @@ public class BusController {
 	}
 
 	@PutMapping("/bus/add_route")
-	public String addBusRoute(String origin,String destination, int id) {
+	public String addBusRoute(@RequestParam String origin, @RequestParam String destination, @RequestParam int id) {
 		return service.addBusRoute(origin, destination, id);
 	}
 //	protected BusController() {
