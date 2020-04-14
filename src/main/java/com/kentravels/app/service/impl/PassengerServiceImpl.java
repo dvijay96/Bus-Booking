ff56910469d538f5c3283d23609718f9675de47f
Digvijay Manoj Parit <EE211385@sasken.com>
2020-04-14 12:45:32 +0530

    remote

--- src/main/java/com/kentravels/app/service/impl/PassengerServiceImpl.java
+++ src/main/java/com/kentravels/app/service/impl/PassengerServiceImpl.java
@@ -46,7 +46,6 @@ public class PassengerServiceImpl implements PassengerService {
 			passenger.setMobileNo(p.getMobileNo());
 			passenger.getBuses().add(bus);
 			passenger.getTickets().add(ticket);
-//			repo.save(passenger);
 
 			String passAdd = busService.addPassenger(passenger, bus.getBusId(), p.getSeats());
 
